e3526d50ad42b53311d31722c7479c7c40147766
Deni Alfaro <denilorenaalfaro@hotmail.com>
2019-05-15 16:15:21 -0700

    Sliders

---
+++ Assets/Shaders/Cabana.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cabana : MonoBehaviour
+{
+    Renderer render;
+
+    [SerializeField]
+    Slider _R;
+    [SerializeField]
+    Slider _G;
+    [SerializeField]
+    Slider _B;
+
+    private void Start()
+    {
+        render = GetComponent<Renderer>();
+
+    }
+
+    private void Update()
+    {
+        render.material.SetFloat("_R", _R.value);
+        render.material.SetFloat("_G", _G.value);
+        render.material.SetFloat("_B", _B.value);
+    }
+
+}
