fafddcb2c1c0df8fa8aab192126148044f572506
Karla-Jaime <carlykhr@gmail.com>
2019-05-22 21:32:53 -0700

    Cambios

---
+++ Assets/Dog.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dog : MonoBehaviour
+{
+    Transform trans;
+    float velocidad;
+        void Start()
+    {
+        velocidad = 1;
+        trans = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        trans.position += Vector3.up * Time.deltaTime * velocidad;
+    
+    }
+}
